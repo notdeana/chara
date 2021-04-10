c76ac85bd51d5677b4ee200c97822bfb3797f5c4
おれわころせ <32080425+notdeana@users.noreply.github.com>
2021-04-10 03:01:36 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,107 +1,140 @@
-// eula
-let eula = [];
-
+// genshin characters
+let chara = [];//{
+  //  name: "✧༺♥༻∞ PLAYER 1 ∞༺♥༻✧",
+    //color: "white"
+//  },
+//  {
+//    name: "*＊✿❀ PLAYER 2 ❀✿＊*",
+    //color: "black"
+//  },
+//  {
+//    name: "｡o°✥✤✣ PLAYER 3  ✣✤✥°o｡",
+    //color: "green"
+//  }
 let randomIndex;
 let animating = false;
-let character = [];
+let characters = [];
 let imageCounter = 0;
-let startRandomizerButton; 
+let startRandomizerbutton;
 let addMoreButton;
 let cnv;
 let nameInputs = [];
 let firstTime = true;
-
-function preload(){
-  
-  for (let i = 0; <= 7; i++) {
-    character[i] = loadImage(`assets/character${i}.JPG`)
+//let newInputButton;
+function preload() {
+  for (let i = 0; i <= 7; i++) {
+    characters[i] = loadImage("assets/character" + i + ".png")
   }
 }
+//let counter = 0;
+
 function setup() {
-  cnv = createCanvas(400, 400);
-  cnv.parent("#canvasDiv")
-  
-  background(300);
-  textSize(32);
-  textFont('Courier new');
+  cnv = createCanvas(1920, 1080);
+  cnv.parent("#canvasDiv");
+  background(220, 700, 800);
+  textSize(24);
   textAlign(CENTER);
-  textStyle(BOLD);
-  fill(255);
   imageMode(CENTER);
-  frameRate(7);
-  
-  //text("Choose Your Character", width/2, height/2);
-  //setInterval(changeBackground, 3000);
-//console.log(character);
-  //button = createButton("CHOOSE YOUR CHARACTER");
-  startRandomizerButton = select('#randButton')
-  startRandomizerButton.mousePressed(buttonPressed);
-  
+  textStyle(ITALIC);
+  fill(70);
+  imageMode(CENTER);
+  frameRate(3);
+  text("✧･ﾟ: *✧･ﾟ:*GENSHIN IMPACT*:･ﾟ✧*:･ﾟ✧", width / 2, height / 2);
+  //button = createButton("∞ ₒ ˚ ° 𐐒 CHOOSE YOUR CHARACTER 𐐚 ° ˚ ₒ ∞");
+  startRandomizerbutton = select('#randButton')
+  startRandomizerbutton.mousePressed(buttonPressed);
   addMoreButton = select('#addMoreButton')
   addMoreButton.mousePressed(addAnotherInput);
-  
-  for (let i = 0; i < 3; i++){
-  nameInputs.push(createInput());
-  nameInputs[nameInputs.length - 1].parents("#inputFields");
+
+  for (let i = 0; i < 3; i++) {
+    nameInputs.push(createInput());
+    nameInputs[nameInputs.length - 1].parent("#inputFields");
   }
-  
+  //console.log(characters);
+  //setTimeout(changeBackground, 1000);
 }
+//console.log(random(3));
+//console.log(chara.length)
+
+//console.log(chara);
+//console.log(chara[2].color);
+//chara[1] = "genshin";
+//console.log("initial array is")
+//console.log(chara);
+
+//chara.shift();
+//console.log("array after shift")
+//console.log(chara);
+
+//chara.splice(4, 1);
+//console.log("array after splice")
+//console.log(chara);
+//chara.unshift("2");
+//console.log("array after unshift")
+//console.log(chara);
+
+
 
 
 function draw() {
-  
-  if (animating == true){
+
+
+  //background(220, 700, 800)
+  if (animating == true) {
     clear();
-   image(character[imageCounter], width/2, height/2);
-    
-    if (imageCounter < character.length - 1){
-    imageCounter++;
+    //ellipse(random(width), random(height), random(10, 100));
+    image(characters[imageCounter], width / 2, height / 2);
+    if (imageCounter < characters.length - 1) {
+      imageCounter++;
       console.log(imageCounter);
     } else {
       imageCounter = 0;
     }
   }
-
-
-//function changeBackground(){
-  //background(random(255), random(255), random(255));
-    //setInterval(changeBackground, 3000);
 }
 function addAnotherInput(){
-   nameInputs.push(createInput());
-  nameInputs[nameInputs.length - 1].parents("#inputFields");
+  nameInputs.push(createInput());
+  nameInputs[nameInputs.length - 1].parent("#inputFields");
 }
-function randomizer(){
+function changeBackground() {
+  //if (counter <= 2){
+  //counter++;
+  //console.log(counter)
+  //background(random(255), random(255), (random(255)));
+  //setTimeout(changeBackground, 1000);
+  //} else {
+
+  //}
+}
+
+function randomizer() {
   animating = false;
-   if (eula[0]){
-      //background(200,650,400);
-     clear();
- randomIndex = int(random(eula.length));
+  if (chara[0]) {
+    // this displays random name and splices it out of array
+    //background(random(220, 255));
+    clear();
+    randomIndex = int(random(chara.length));
 
-console.log(eula[randomIndex].name);
-text(eula[randomIndex].name, width/2, height - 50);
-     image(random(character), width/2, height/2);
-
-eula.splice(randomIndex, 1);
-console.log(eula);
-
-} else {
-  background(200,650,400);
-  text("game over!", 50, 50);
-}
+    //console.log(chara[randomIndex].name);
+    //text(chara[randomIndex].name + "'s favorite color is" + chara[randomIndex].color, 50, 50);
+    text(chara[randomIndex], width / 2, height - 50);
+    image(random(characters), width / 2, height / 2);
+    chara.splice(randomIndex, 1);
+  } else {
+    background(random(220, 255));
+    text("GAME OVER", width / 2, height / 2);
+  }
 }
 
 function buttonPressed() {
   if (firstTime == true){
-    
-  
-  for (let i = 0; i < nameInputs.length; i++){
-    eula.push(nameInputs[i].value());
-    }
-    firstTime = false;
+    //for (let i = 0; i < nameInputs.length; i)
+  for (let i= 0; i < nameInputs.length; i++){
+    chara.push(nameInputs[i].value());
   }
-  
+  firstTime = false;
+} //else
   animating = true;
   setTimeout(randomizer, 2000);
- 
+
 }
